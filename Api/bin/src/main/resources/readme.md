d3b8dd5c1cc05948ae4cff6f29007b53f4fda8fe
강민광 <69151872+MinKongMan@users.noreply.github.com>
2022-10-06 22:44:29 +0900

    Delete Api directory

--- Api/bin/src/main/resources/readme.md
+++
@@ -1,3 +0,0 @@
-카카오 2차 테스트 준비 프로젝트입니다.
-
-url: https://programmers.co.kr/skill_check_assignments/67
--- Api/src/main/java/Global.java
+++
@@ -1,8 +0,0 @@
-public class Global {
-    public static final String X_AUTH_TOKEN = "6b9e1bf4c23ef73f64f3a3e820f049e2";
-    public static final String BASE_URL = "https://huqeyhi95c.execute-api.ap-northeast-2.amazonaws.com/prod";
-    public static final String START = "/start";
-    public static final String WAIT = "/waiting_line";
-    public static final String RESULT = "/game_result";
-    public static final String User = "/user_info";
-}
